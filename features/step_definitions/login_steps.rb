6b3fd3004caa5dcff104bc8d777c0ccfb63d0d85
Gatis <gatis.vaitovskis@testdevlab.com>
2017-04-25 19:06:23 +0300

    lecture3

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,13 +1,16 @@
 When(/^I am on Appimation home page/) do
   visit('/')
-  
+  find(:id,'start_button').visible?
+  find(:id,'signup-b').visible?
+  find(:id,'login-b').visible?
 end
 
 Then(/^I click Try Now/) do
-  
+  find(:id,'start_button').click
+
 end
 Then(/^I enter (.*) in SignUp email/) do
-  
+  find(:type,'email').send_keys('tetetetetets')
 end
 
 Then(/^I enter (.*) in SignUp passwords/) do
