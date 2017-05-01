cb7722becd934141091a2cbaa459709631a58035
Gatis <gatis.vaitovskis@testdevlab.com>
2017-05-01 22:35:17 +0300

    homework 3.1

--- features/demo.feature
+++ features/demo.feature
@@ -2,8 +2,8 @@ Feature: Demo Feature
 
     Scenario: Open SignUp Page
         Given I am on Appimation home page
-        When I click Try Now
-        And I enter testaepasts@tests.com in SignUp email
-        And I enter testParole123 in SignUp passwords
-        And I enter demoProject in SignUp project name
-        Then I cancel SignUp
+        When I click Login button
+        And I enter gatis.vaitovskis@testdevlab.com in email field
+        And I enter password testtest in password field
+        Then I press login button
+        
--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -3,25 +3,25 @@ When(/^I am on Appimation home page/) do
   find(:id,'start_button').visible?
   find(:id,'signup-b').visible?
   find(:id,'login-b').visible?
-end
-
-Then(/^I click Try Now/) do
-  find(:id,'start_button').click
 
 end
-Then(/^I enter (.*) in SignUp email/) do |email|
-  find(:xpath,'//div[@id="signup"]/descendant::input[@name = "login"]').send_keys email
+
+Then(/^I click Login button/) do
+  find(:id,'login-b').click
+
 end
 
-Then(/^I enter (.*) in SignUp passwords/) do |password|
-  find(:xpath,'//div[@id="signup"]/descendant::input[@name = "password1"]').send_keys password
-  find(:xpath,'//*[@id="signup"]/descendant::input[@name = "password2"]').send_keys password
+Then(/^I enter (.*) in email field/) do |email|
+  find(:xpath,'//div[@id="login"]/descendant::input[@name = "login"]').send_keys email
+
 end
 
-Then(/^I enter (.*) in SignUp project name/) do |name|
-  find(:xpath,'//div[@id="signup"]/descendant::input[@name = "project_name"]').send_keys name
+Then(/^I enter password (.*) in password field/) do |password|
+  find(:xpath,'//div[@id="login"]/descendant::input[@name = "password"]').send_keys password
+
 end
 
-Then(/^I cancel SignUp/) do
-    find(:xpath,'//div[@id="signup"]/descendant::img[@class = "closecross"]').click
+Then(/^I press login button/) do
+  find(:xpath,'//div[@id="login"]/descendant::button[@class = "button button-block innerButton"]').click
+
 end
